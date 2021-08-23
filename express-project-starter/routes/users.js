395c33787550e3956f1241349bf778b3f9bacc86
chris <felix.christopher.e@gmail.com>
2021-08-23 15:47:34 -0700

    user validators to be added

--- express-project-starter/routes/users.js
+++ express-project-starter/routes/users.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 const db = require("../db/models")
 const { csrfProtection, asyncHandler} = require('./utils');
-const { loginUser, logoutUser } = require('../auth');
+// const {  logoutUser } = require('../auth');
 const { check, validationResult } = require('express-validator');
 const bcrypt = require('bcryptjs');
 
