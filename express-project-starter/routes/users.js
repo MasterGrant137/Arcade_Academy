c99cb636be0da9df47e357440dc76d7cedf2953c
chris <felix.christopher.e@gmail.com>
2021-08-23 15:29:37 -0700

    user.id error

--- express-project-starter/routes/users.js
+++ express-project-starter/routes/users.js
@@ -1,12 +1,20 @@
 var express = require('express');
 var router = express.Router();
 const db = require("../db/models")
-
+const { csrfProtection, asyncHandler} = require('./utils');
+const { loginUser, logoutUser } = require('../auth');
+const { check, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+router.get('/register', csrfProtection, asyncHandler (async(req, res) => {
+  res.render("register.pug", { csrfToken: req.csrfToken(), title: "Register" });
+}));
+
 
 
 module.exports = router;
