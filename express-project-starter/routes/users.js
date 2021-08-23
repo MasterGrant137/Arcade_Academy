b0f2b8cc41e00ada1cb7632b125f60f81df28f9d
chris <felix.christopher.e@gmail.com>
2021-08-23 14:45:42 -0700

    setup

--- express-project-starter/routes/users.js
+++ express-project-starter/routes/users.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
+const db = require("../db/models")
+
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
 
 module.exports = router;
---
+++ express-project-starter/routes/utils.js
@@ -0,0 +1,3 @@
+const csrf = require('csurf');
+const csrfProtection = csrf({cookie: true});
+const asyncHandler = (handler) => (req, res, next) => handler(req, res, next).catch(next);
