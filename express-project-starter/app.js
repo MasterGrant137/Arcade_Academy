a303b69f5c6faf03735324d99663c467f6d5d71d
aderocher <am.derocher@gmail.com>
2021-08-24 15:34:17 -0700

    done with games and id routes

--- express-project-starter/app.js
+++ express-project-starter/app.js
@@ -9,7 +9,7 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const { restoreUser } = require('./auth');
-// const gamesRouter = require('./routes/games')
+const gamesRouter = require('./routes/games')
 
 const app = express();
 
@@ -40,7 +40,7 @@ store.sync();
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-// app.use('/games', gamesRouter);
+app.use('/games', gamesRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
--- express-project-starter/routes/games.js
+++ express-project-starter/routes/games.js
@@ -1,1 +1,19 @@
-const express = require('express')
+const express = require('express');
+const router = express.Router();
+const { Game } = require('../db/models');
+const { asyncHandler } = require('./utils');
+
+router.get('/', asyncHandler( async (req, res, next) => {
+  const games = await Game.findAll();
+  res.render('home.pug', { title: 'Arcade Academy', games });
+}));
+
+router.get(`/:id(\\d+)`, asyncHandler( async (req, res, next) => {
+  const gameId = parseInt(req.params.id, 10)
+  const game = await Game.findByPk(gameId);
+
+  res.render('game.pug', { title: `AA-${game.name}`, game });
+}));
+
+
+module.exports = router;
