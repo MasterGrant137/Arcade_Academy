c4eb051e52dbe32fdbee834e79d89dbedac67153
Alejandro C. Grant <authorcgrant@gmail.com>
2021-08-26 09:07:40 -0700

    got reviews to go to db

--- routes/games.js
+++ routes/games.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { Game } = require('../db/models');
+const { Review } = require('../db/models');
 const { asyncHandler } = require('./utils');
 const { csrfProtection } = require('./utils');
 const { check, validationResult } = require('express-validator');
@@ -45,14 +46,18 @@ const reviewsValidators = [
 ];
 
 
-router.post("/:id(\\d+)", requireAuth, csrfProtection, reviewsValidators,  asyncHandler(async(req, res)=> {
-  const userId = req.session.auth.userId
+router.post("/:id(\\d+)", requireAuth, csrfProtection, reviewsValidators, asyncHandler(async(req, res)=> {
+  const userId = req.session.auth.userId;
   const { gameId, content} = req.body;
-  console.log(gameId, userId)
-  // const newReview = await Review.create({
-  //   content,
-  //   game_id: gameId
-  // });
+
+console.log(gameId, userId);
+
+  await Review.create({
+    user_id: userId,
+    content,
+    game_id: gameId
+  });
+
   const validationErrors = validationResult(req);
   if (validationErrors.isEmpty()) {
       //where successful review posts will go
@@ -61,10 +66,10 @@ router.post("/:id(\\d+)", requireAuth, csrfProtection, reviewsValidators,  async
     res.render('game.pug', { title: 'test', gameId, csrfToken:req.csrfToken(), errors });
 
   }
+
   // res.redirect('/')
   // res.render('/:id(\\d+)', { title: `AA-${game.name}`, csrfToken: req.csrfToken() });
 }));
-
 
 router.get('/topGames', asyncHandler(async(req,res) => {
   const topGames = await Game.findAll({ limit: 10 });
